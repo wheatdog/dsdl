7ca6abb252bf5f462ffdcbad86722721849debd6
wheatdog <wheatdoge@gmail.com>
2016-05-02 20:23:02 +0800

    fix bugs

--- s12bittobcd.v
+++ s12bittobcd.v
@@ -32,18 +32,19 @@ module s12bittobcd(sign, out_flat, binary);
              out[i] = 4'd0;
           end
 
-        for (j = 10; j >= 0; j = j - 1)
+        for (j = 11; j >= 0; j = j - 1)
           begin
              for (i = 0; i < 4; i = i + 1)
                begin
                   if (out[i] > 4) out [i] = out[i] + 3;
                end
 
-             for (i = 3; i >= 0; i = i - 1)
+             for (i = 3; i > 0; i = i - 1)
                begin
                   out[i] = out[i] << 1;
                   out[i][0] = out[i - 1][3];
                end
+             out[0] = out[0] << 1;
              out[0][0] = temp[j];
           end
      end
